f42acd4754d861ff8fec0a4d1656d546ec759a44
wmjungmann <wmjungmann@gmail.com>
2023-05-27 16:32:14 +0900

    Isaac so lazy this guy does nothing resnet32

--- BB_Website/js/script.js
+++ BB_Website/js/script.js
@@ -1,24 +1,63 @@
-//Button red/green colour changer
-function colorChange(id) {
+document.addEventListener('DOMContentLoaded', function() {
+  // Button red/green color changer
+  function colorChange(id) {
     var el = document.getElementById(id);
     var currentClass = el.getAttribute("class");
-    if(currentClass == 'workoutIncomplete workoutButton') {
+    if (currentClass === "workoutIncomplete workoutButton") {
       el.setAttribute("class", "workoutComplete workoutButton");
-    }
-    else {
+    } else {
       el.setAttribute("class", "workoutIncomplete workoutButton");
     }
-}
+  }
 
+  // Stopwatch function
+  let stopBtn = document.getElementById('stop');
+  let startBtn = document.getElementById('start');
 
+  let minute = 0;
+  let second = 0;
+  let hour = 0;
+  let count = 0;
+  let timer = false;
 
+  stopBtn.addEventListener('click', function () {
+    timer = false;
+  });
 
-//Stop Wactch function
-let stopBtn = document.getElementById('stop');
+  startBtn.addEventListener('click', function () {
+    timer = true;
+    stopWatch();
+  });
 
-let minute = 00;
-let second = 00;
+  function stopWatch() {
+    if (timer) {
+      count++;
 
-stopBtn.addEventListener('click', function () {
-  timer = false;
+      if (count === 100) {
+        second++;
+        count = 0;
+      }
+
+      if (second === 60) {
+        minute++;
+        second = 0;
+      }
+
+      if (minute === 60) {
+        hour++;
+        minute = 0;
+        second = 0;
+      }
+
+      let hrString = hour.toString().padStart(2, '0');
+      let minString = minute.toString().padStart(2, '0');
+      let secString = second.toString().padStart(2, '0');
+      let countString = count.toString().padStart(2, '0');
+
+      document.getElementById('min').innerHTML = minString;
+      document.getElementById('sec').innerHTML = secString;
+
+      setTimeout(stopWatch, 10);
+    }
+  }
 });
